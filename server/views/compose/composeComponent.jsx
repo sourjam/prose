aa290688fd6cd08b21ecb26112eed777df82eb3c
Jim Yang <jimyyang@gmail.com>
2016-11-22 11:33:12 -0500

    adding read component

--- server/views/compose/composeComponent.jsx
+++ server/views/compose/composeComponent.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RTE from 'react-rte';
+import Read from './readComponent'
 // import {Editor, EditorState, RichUtils} from 'draft-js';
 
 console.log('compose')
+
 
 export class Well extends React.Component {
   constructor(props) {
@@ -11,32 +13,33 @@ export class Well extends React.Component {
     let reg = /\<.>|<\/.>|\s/g
     let wellArr = props.content.split(reg);
     this.state = {
-      content: wellArr
+      content: wellArr,
+      editWell: null
     }
     console.log('well props', this.state)
   }
 
-  makeWell(index) {
+  editWell(index) {
     console.log(index, this.state.content[index])
+    // this.setState({editWell: index})
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.props.saveWell}>Save Well</button>
         {this.state.content.map((val, index, arr) => {
           return (
-            <span>
-              {val !== '' ?
-                <button onClick={this.makeWell.bind(this, index)}>{val}</button>
-                : null}
+            <span key={index}>
+              {val !== '' && val !== '<br>' ?
+                <button onClick={this.editWell.bind(this, index)}>{val}</button> : null}
+              { this.state.editWell === index ?
+                <Editor/> : null}
             </span>
           )
         })}
       </div>
     )
   }
-
 }
 
 export default class Editor extends React.Component {
@@ -52,12 +55,11 @@ export default class Editor extends React.Component {
     testInit: [{html: '<p>Hello'}, {html: 'my'}, {html: 'baby,</p>'}, {html: '<p>hello'}, {html: 'my'}, {html: 'honey!</p?'}],
     value: RTE.createEmptyValue(),
     testValue: RTE.createValueFromString('<p>hello there again</p><p>guess whos been</p>', 'html'),
-    prose: true
+    mode: 'prose',
   }
 
   onChange = (value) => {
     this.setState({value});
-    console.log('hi', this.state.value)
     if (this.props.onChange) {
       this.props.onChange(
         value.toString('html')
@@ -79,11 +81,10 @@ export default class Editor extends React.Component {
     return well;
   }
 
-  swapMode() {
-    this.setState({prose: !this.state.prose})
+  swapMode(mode) {
+    this.setState({mode: mode})
     // create/convert prose to well
-
-
+    this.saveProse();
   }
 
   checkState() {
@@ -108,105 +109,32 @@ export default class Editor extends React.Component {
       <div>
         <button onClick={this.checkState.bind(this)}>Check State</button>
         <button onClick={this.saveProse.bind(this)}>Save</button>
-        <button onClick={this.swapMode.bind(this)}>Swap Mode</button>
-        { this.state.prose === true ?
+        <button onClick={this.swapMode.bind(this, 'prose')}>Prose Mode</button>
+        <button onClick={this.swapMode.bind(this, 'well')}>Well Mode</button>
+        <button onClick={this.swapMode.bind(this, 'read')}>Read Mode</button>
+        { this.state.mode === 'prose' ?
           <RTE
             value={this.state.value}
             onChange={this.onChange.bind(this)}
           />
           :
+          null
+        }
+        { this.state.mode === 'well' ?
           <div>
             <Well saveWell={this.saveWell.bind(this)} content={this.state.value.toString('html')} />
           </div>
+          :
+          null
+        }
+        { this.state.mode === 'read' ?
+          <Read></Read>
+          :
+          null
         }
       </div>
     )
   }
 }
 
-
-
-export class Compose extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      content: [
-        {text: 'Text'}, {text: 'goes'}, {text: '<b>here</b>'}
-      ],
-      prose: '',
-      layers: [
-
-      ]
-    }
-  }
-
-  componentDidMount() {
-    let p = this.state.content.reduce((memo, val) => {
-      if (typeof memo === 'object') {
-        memo = memo.text
-      }
-      return memo = memo + ' ' + val.text;
-    })
-    console.log('memo', p)
-    this.setState({prose: p})
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    console.log('update', nextProps, nextState)
-  }
-
-
-  render() {
-    return (
-      <div id="prose">
-
-      </div>
-    )
-  }
-}
-
 ReactDOM.render(<Editor />, document.getElementById('compose'));
-
-// export class Draft extends React.Component {
-//   constructor(props){
-//     super(props);
-//     this.state = {editorState: EditorState.createEmpty()};
-//     this.onChange = (editorState) => this.setState({editorState});
-//     this.handleKeyCommand = this.handleKeyCommand.bind(this);
-//     console.log('props', props)
-//   }
-
-//   handleKeyCommand(command) {
-//     const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-//     if (newState) {
-//       this.onChange(newState);
-//       return 'handled';
-//     }
-//     return 'not-handled';
-//   }
-
-//   _onBoldClick() {
-//     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-//   }
-
-//   componentDidMount() {
-//     console.log('compose mounted')
-//   }
-//   clickHandler() {
-//     console.log(this.state.editorState.getCurrentContent())
-//   }
-//   render() {
-//     const {editorState} = this.state;
-//     return (
-//       <div>
-//         <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-//         <button onClick={this.clickHandler.bind(this)}>EditorState</button>
-//         <Editor
-//           editorState={editorState}
-//           handleKeyCommand={this.handleKeyCommand}
-//           onChange={this.onChange}
-//         />
-//       </div>
-//     )
-//   }
-// }
---
+++ server/views/compose/readComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class Read extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div>Reading</div>
+  }
+}
